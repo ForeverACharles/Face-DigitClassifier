d1ed54aa1571f33169ec995f0485db4f1bcbaf90
cl1217 <cl1217@scarletmail.rutgers.edu>
2021-11-29 22:16:52 -0500

    updated read and runner to modularize function calls

--- read.py
+++ read.py
@@ -1,15 +1,18 @@
 import os
 
 def read_data(data_path, labels_path):
+
+    print("Reading in dataset...")
+
     # get labels
     labels = open(labels_path, 'r').readlines()
     #should be 150 for facedata\facedatatestlabels, should be 1000 for digitdata/testdata
-    print(len(labels))
+    #print(len(labels))
 
     #get data. f contains all lines
     f = open(data_path, 'r').readlines()
     height = int(len(f)/len(labels))  #height of each image = total num of lines / number of labels
-    print(height)
+    #print(height)
 
     #data will be a 3D array of the faces
     data = []
@@ -30,20 +33,21 @@ def print_dataset(data, percent):
         print_entry(data, i)
         print("-----------------------------------------------------------")
 
-def read_datasets(DIGITS_DATA_PATH, DIGITS_LABEL_PATH, FACES_DATA_PATH, FACES_LABEL_PATH):
+#def read_datasets(DIGITS_DATA_PATH, DIGITS_LABEL_PATH, FACES_DATA_PATH, FACES_LABEL_PATH):
+def read_dataset(DATA_PATH, LABEL_PATH):
     #change paths to whatever you need it to be
     
 
-    digits, digits_labels = read_data(DIGITS_DATA_PATH, DIGITS_LABEL_PATH)
-    faces, faces_labels = read_data(FACES_DATA_PATH, FACES_LABEL_PATH)
+    data, data_labels = read_data(DATA_PATH, LABEL_PATH)
+    #faces, faces_labels = read_data(FACES_DATA_PATH, FACES_LABEL_PATH)
 
     #print_entry(numbers, 72)
     #print_entry(faces, 35)
     #print_dataset(faces, 0.1)
 
     #dataset can be updated to store more information than just the raw data and their correct labels
-    digits_dataset = [digits, digits_labels]
-    faces_dataset = [faces, faces_labels]
+    dataset = [data, data_labels]
+    #digits_dataset = [digits, digits_labels]
+    #faces_dataset = [faces, faces_labels]
 
-    return digits_dataset, faces_dataset
-
+    return dataset
--- runner.py
+++ runner.py
@@ -1,19 +1,25 @@
-from read import read_datasets, print_entry, print_dataset
+from read import read_dataset, print_entry, print_dataset
 from perceptron import p_train, p_evaluate
 
 import os
 
 def main():
-    #curr_dir = os.path.dirname(__file__) + "\\"
-    DIGITS_TEST_DATA =  "data\\digitdata\\testimages"
-    DIGITS_TEST_LABELS = "data\\digitdata\\testlabels"
-    FACES_DATA_PATH = "data\\facedata\\facedatatest"
-    FACES_LABEL_PATH =  "data\\facedata\\facedatatestlabels"
-    DIGITS_TRAIN_DATA =  "data\\digitdata\\trainingimages"
-    DIGITS_TRAIN_LABEL =   "data\\digitdata\\traininglabels"
-    digits_train_dataset, faces_dataset = read_datasets(DIGITS_TRAIN_DATA, DIGITS_TRAIN_LABEL, FACES_DATA_PATH, FACES_LABEL_PATH)
-    digits_dataset, faces_dataset = read_datasets(DIGITS_TEST_DATA, DIGITS_TEST_LABELS, FACES_DATA_PATH, FACES_LABEL_PATH)
-    #our testing will have us limit the percent of the dataset to perform training on
+    curr_dir = os.path.dirname(__file__) + "\\"
+
+    DIGITS_TRAIN_DATA =  curr_dir +  "data\\digitdata\\trainingimages"
+    DIGITS_TRAIN_LABELS =   curr_dir +  "data\\digitdata\\traininglabels"
+    DIGITS_TEST_DATA =  curr_dir + "data\\digitdata\\testimages"
+    DIGITS_TEST_LABELS = curr_dir +  "data\\digitdata\\testlabels"
+    
+    FACES_TRAIN_DATA =  curr_dir + "data\\facedata\\facedatatrain"
+    FACES_TRAIN_LABELS =  curr_dir +  "data\\facedata\\facedatatrainlabels"
+    FACES_TEST_DATA =  curr_dir + "data\\facedata\\facedatatest"
+    FACES_TEST_LABELS =  curr_dir +  "data\\facedata\\facedatatestlabels"
+   
+   
+    digits_train_dataset = read_dataset(DIGITS_TRAIN_DATA, DIGITS_TRAIN_LABELS)
+    digits_dataset = read_dataset(DIGITS_TEST_DATA, DIGITS_TEST_LABELS)
+    
     #digits_dataset = trim_dataset(digits_dataset, 1)
 
     #print_dataset(digits_dataset[0], 0.1)
@@ -24,6 +30,7 @@ def main():
     #p_train(faces_dataset)
     #p_evaluate(faces_dataset)
 
+#our testing will have us limit the percent of the dataset to perform training on
 def trim_dataset(dataset, percent):
     size = int(len(dataset[0]) * percent)
     for i in range(len(dataset)):
